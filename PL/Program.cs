7fd33d24d0699af60c32817bdbe2c4b23d3b7a6e
digis <digis@LAPTOP-AD7LFKO5>
2023-12-28 14:00:23 -0600

    SMTP y Login

--- PL/Program.cs
+++ PL/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
